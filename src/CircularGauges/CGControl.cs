dc5c23a893d398855b41248ab5b3dafaf5659fd5
EugeneB <eugene_l@tut.by>
2018-09-12 23:57:49 +0300

    add: Demo

---
+++ src/CircularGauges/CGControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircularGauges
+{
+    enum EnumCenterPos
+    {
+        CenterLeft,
+        CenterCenter,
+        CenterRight,
+        BottomLeft,
+        BottomCenter,
+        BottomRight
+    };
+
+    public partial class CGControl : UserControl
+    {
+        public CGControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+    }
+}
